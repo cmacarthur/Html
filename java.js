c5d8a2ecd046e1f3bc752519f8f96d7fdbfca0fb
cmacarthur <790024@seq.org>
2016-05-26 07:59:46 -0700

    Add files via upload

--- java.js
+++ java.js
@@ -1,8 +1,13 @@
 
 
 
-var randomlinks=new Array()
+var MAX_NUMBERS=10;
 
+var nums=[];
+for(var i=0; i<MAX_NUMBERS;i++)
+{
+nums[i]= Math.floor(Math.random()*10);
+}
 randomlinks[0]="http://thatsthefinger.com/"
 randomlinks[1]="http://heeeeeeeey.com/"
 randomlinks[2]="http://www.staggeringbeauty.com/"
@@ -16,11 +21,54 @@ window.location=randomlinks[Math.floor(Math.random()*randomlinks.length)]
 }
 
 function getValue(){
-    var retVal = new Array()
-    retVal = prompt("Enter numbers : ", "");
-   var total=0;
-  for(var i=0; i<retVal.length;i++){
-   total += retvalue[i];
+nums.sort();
+document.write(nums)
+
   }
-  document.write( responses[1]);
+function Average(){
+nums.sort();
+var total = 0;
+var length = 0;
+
+for(var i = 0; i < nums.length; i++){
+total += nums[i];
+console.log(nums);
 }
+length = total/nums.length
+document.write(length);
+}
+
+function median() {
+
+var half = Math.floor(nums.length/2);
+//document.write(half);
+if(nums.length % 2)
+{
+    document.write(nums[half]);
+
+}
+else{
+  document.write((nums[half-1] + nums[half]) / 2.0);
+}
+}
+function mode(){
+
+  var numMapping = {};
+    for(var i = 0; i < nums.length; i++){
+        if(numMapping[nums[i]] === undefined){
+            numMapping[nums[i]] = 0;
+        }
+            numMapping[nums[i]] += 1;
+    }
+    var greatestFreq = 0;
+    var mode;
+    for(var prop in numMapping){
+        if(numMapping[prop] > greatestFreq){
+            greatestFreq = numMapping[prop];
+            mode = prop;
+        }
+    }
+    document.write(parseInt(mode));
+}
+
+//console.log(getValue());
